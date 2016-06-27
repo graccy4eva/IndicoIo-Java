5973e08f486f79cbf2a1a44ec7d2f9c23973cc4c
Anne Carlson <anne_carlson@brown.edu>
2016-06-27 13:41:48 -0400

    added version1 test for NER

--- test/io/indico/test/TestPlaces.java
+++ test/io/indico/test/TestPlaces.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 
 import io.indico.Indico;
 import io.indico.api.results.BatchIndicoResult;
@@ -37,11 +38,52 @@ public class TestPlaces {
     }
 
     @Test
+    public void testSingleV1() throws IOException, IndicoException {
+        Indico test = new Indico(new File("config.properties"));
+        Map params = new HashMap();
+        params.put("version", 1);
+
+        String example = "Lets all go to Virginia beach before it gets too cold to wander outside.";
+        List<Map<String, Object>> results = test.places.predict(example, params).getPlaces();
+
+        boolean expected = false;
+        for (Map<String, Object> obj : results) {
+            if (obj.get("text").equals("Virginia")) {
+                expected = true;
+                break;
+            }
+        }
+
+        assertTrue(expected);
+    }
+
+    @Test
     public void testBatch() throws IOException, IndicoException {
         Indico test = new Indico(new File("config.properties"));
 
         final String example = "Lets all go to Virginia beach before it gets too cold to wander outside.";
         BatchIndicoResult result = test.places.predict(new String[] {example, example});
+        List<List<Map<String, Object>>> results = result.getPlaces();
+        assertTrue(results.size() == 2);
+        boolean expected = false;
+        for (Map<String, Object> obj : results.get(0)) {
+            if (obj.get("text").equals("Virginia")) {
+                expected = true;
+                break;
+            }
+        }
+
+        assertTrue(expected);
+    }
+
+    @Test
+    public void testBatchV1() throws IOException, IndicoException {
+        Indico test = new Indico(new File("config.properties"));
+        Map params = new HashMap();
+        params.put("version", 1);
+
+        final String example = "Lets all go to Virginia beach before it gets too cold to wander outside.";
+        BatchIndicoResult result = test.places.predict(new String[] {example, example}, params);
         List<List<Map<String, Object>>> results = result.getPlaces();
         assertTrue(results.size() == 2);
         boolean expected = false;
