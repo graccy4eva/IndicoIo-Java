c3a6d99ca6a2e12028ece9ea6caccf4ece55d1a9
Chris Lee <sihrc.c.lee@gmail.com>
2016-07-01 18:18:15 -0400

    ADD: Custom permissioning

--- src/io/indico/api/custom/IndicoCollection.java
+++ src/io/indico/api/custom/IndicoCollection.java
@@ -2,6 +2,7 @@ package io.indico.api.custom;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -18,13 +19,20 @@ import io.indico.api.utils.IndicoException;
  */
 public class IndicoCollection {
     CustomApiClient client;
-    String collectionName;
-    String domain;
+    Map<String, Object> configs;
 
-    public IndicoCollection(CustomApiClient client, String collectionName, String domain) {
-        this.collectionName = collectionName;
+    public IndicoCollection(CustomApiClient client, final String collectionName, final String domain, final boolean shared) {
         this.client = client;
-        this.domain = domain;
+        configs = new HashMap<String, Object>() {{
+            put("collection", collectionName);
+            if (domain != null)
+                put("domain", domain);
+            put("shared", shared);
+        }};
+    }
+
+    public IndicoCollection(CustomApiClient client, final String collectionName, final String domain) {
+        this(client, collectionName, domain, false);
     }
 
     public String addData(List<CollectionData> examples) throws IOException, IndicoException {
@@ -32,15 +40,54 @@ public class IndicoCollection {
         for (CollectionData data : examples) {
             postPackage.add(new String[] {data.data, data.result.toString()});
         }
-        return this.client.addData(this.collectionName, postPackage, this.domain);
+        return this.client.addData(postPackage, this.configs);
     }
 
     public String train() throws IOException, IndicoException {
-        return this.client.train(this.collectionName);
+        return this.client.train(this.configs);
     }
 
     public String clear() throws IOException, IndicoException {
-        return this.client.clear(this.collectionName);
+        return this.client.clear(this.configs);
+    }
+
+    public String register() throws IOException, IndicoException {
+        return this.register(false);
+    }
+
+    public String register(boolean makePublic) throws IOException, IndicoException {
+        Map<String, Object> data = new HashMap<>();
+        data.putAll(this.configs);
+        data.put("make_public", makePublic);
+        return this.client.register(data);
+    }
+
+    public String deregister() throws IOException, IndicoException {
+        return this.client.deregister(this.configs);
+    }
+
+    public String authorize(String email, Permission permission) throws IOException, IndicoException {
+        Map<String, Object> data = new HashMap<>();
+        data.putAll(this.configs);
+        data.put("email", email);
+        data.put("permission_type", permission.toString());
+        return this.client.authorize(data);
+    }
+
+    public String deauthorize(String email) throws IOException, IndicoException {
+        Map<String, Object> data = new HashMap<>();
+        data.putAll(this.configs);
+        data.put("email", email);
+        return this.client.deauthorize(data);
+    }
+
+    public String rename(String newName) throws IOException, IndicoException {
+        Map<String, Object> data = new HashMap<>();
+        data.putAll(this.configs);
+        data.put("name", newName);
+        String result = this.client.rename(data);
+        this.configs.put("collection", newName);
+        return result;
     }
 
     public String removeExamples(List<CollectionData> data) throws IOException, IndicoException {
@@ -49,17 +96,17 @@ public class IndicoCollection {
             postData.add(entry.data);
         }
 
-        return this.client.removeExamples(this.collectionName, postData);
+        return this.client.removeExamples(postData, this.configs);
     }
 
     public Map<String, ?> info() throws IOException, IndicoException {
-        return this.client.info(this.collectionName);
+        return this.client.info(this.configs);
     }
 
     public void waitUntilReady(long interval) throws IOException, IndicoException {
         String status;
         while (true) {
-            status = this.client.info(this.collectionName).get("status").toString();
+            status = this.info().get("status").toString();
             if (Objects.equals(status, "ready"))
                 return;
             if (!Objects.equals(status, "training"))
@@ -72,13 +119,17 @@ public class IndicoCollection {
         }
     }
 
+    public String name() {
+        return configs.get("collection").toString();
+    }
+
     public List<?> predict(List<String> data) throws IOException, IndicoException {
         List<String> postData = ImageUtils.convertToImages(data, Api.CUSTOM.getSize(null), (boolean) Api.CUSTOM.get("minResize"));
-        return this.client.predict(this.collectionName, postData, this.domain);
+        return this.client.predict(postData, this.configs);
     }
 
     public Object predict(String data) throws IOException, IndicoException {
         String postData = ImageUtils.convertToImage(data, Api.CUSTOM.getSize(null), (boolean) Api.CUSTOM.get("minResize"));
-        return this.client.predict(this.collectionName, postData, this.domain);
+        return this.client.predict(postData, this.configs);
     }
 }
